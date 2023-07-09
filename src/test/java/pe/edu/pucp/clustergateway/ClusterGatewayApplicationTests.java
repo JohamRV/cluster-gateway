739ca67c288ffb7b9832bfdc0883c9eae0e55252
Joham Romucho <joham.romucho@pucp.edu.pe>
2023-07-08 23:54:05 -0500

    feat: set up cluster gateway

---
+++ src/test/java/pe/edu/pucp/clustergateway/ClusterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.clustergateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
